34545ec69d0b9e15c50d4ca70bb72218191c4f68
william-francia <wfranciapoma@byupathway.edu>
2026-03-09 14:15:38 -0400

    changes

--- server.js
+++ server.js
@@ -1,39 +1,51 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
+ * Primary server file
+ ******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
-const env = require("dotenv").config()
+require("dotenv").config()
+
 const app = express()
+
+/* ***********************
+ * Static Files
+ *************************/
 app.use(express.static("public"))
-const static = require("./routes/static")
+
+/* ***********************
+ * View Engine
+ *************************/
 const expressLayouts = require("express-ejs-layouts")
+
 app.set("view engine", "ejs")
+app.set("views", "./views")
+
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
+app.set("layout", "layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-// Index Route
+const staticRoutes = require("./routes/static")
+app.use(staticRoutes)
+
+// Home Route
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
-});
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const PORT = process.env.PORT || 3000; // Usa 3000 si no hay PORT definido
-const HOST = process.env.HOST || "0.0.0.0"; // 0.0.0.0 funciona en Rende
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || "0.0.0.0"
 
 /* ***********************
- * Log statement to confirm server operation
+ * Start Server
  *************************/
 app.listen(PORT, HOST, () => {
-  console.log(`App listening on ${HOST}:${PORT}`);
-});
+  console.log(`Server running at http://${HOST}:${PORT}`)
+})
