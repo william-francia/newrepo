b4a8dbc425f0a7b337794b301a09c43a6fea0662
william-francia <wfranciapoma@byupathway.edu>
2026-03-09 14:56:54 -0400

    changes

--- routes/static.js
+++ routes/static.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use(express.static(path.join(__dirname, "..", "public")));
+router.use("/css", express.static(path.join(__dirname, "..", "public", "css")));
+router.use("/js", express.static(path.join(__dirname, "..", "public", "js")));
+router.use("/images", express.static(path.join(__dirname, "..", "public", "images")));
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,28 +1,51 @@
+/* ******************************************
+ * Primary server file
+ ******************************************/
+
+/* ***********************
+ * Require Statements
+ *************************/
 const express = require("express")
+const path = require("path")
 require("dotenv").config()
 
 const app = express()
 
-/* Static files */
-app.use(express.static("public"))
+/* ***********************
+ * Static Files
+ *************************/
+app.use(express.static(path.join(__dirname, "public")))
 
-/* View engine */
+/* ***********************
+ * View Engine
+ *************************/
 const expressLayouts = require("express-ejs-layouts")
 
 app.set("view engine", "ejs")
-app.set("views", "./views")
+app.set("views", path.join(__dirname, "views"))
 
 app.use(expressLayouts)
 app.set("layout", "layouts/layout")
 
-/* Routes */
+/* ***********************
+ * Routes
+ *************************/
+const staticRoutes = require("./routes/static")
+app.use(staticRoutes)
+
+// Home Route
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" })
 })
 
-/* Server */
+/* ***********************
+ * Local Server Information
+ *************************/
 const PORT = process.env.PORT || 3000
 
+/* ***********************
+ * Start Server
+ *************************/
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
