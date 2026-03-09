15e5f14a1e1346100def2c487bb41e3890004d3d
william-francia <wfranciapoma@byupathway.edu>
2026-03-09 15:56:47 -0400

    Changes

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-/* ******************************************
- * Primary server file
- ******************************************/
 
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
 const path = require("path")
 require("dotenv").config()
 
 const app = express()
 
-/* ***********************
- * Static Files
- *************************/
+/* Static Files*/
 app.use(express.static(path.join(__dirname, "public")))
 
-/* ***********************
- * View Engine
- *************************/
+/*View Engine*/
 const expressLayouts = require("express-ejs-layouts")
 
 app.set("view engine", "ejs")
@@ -27,9 +17,7 @@ app.set("views", path.join(__dirname, "views"))
 app.use(expressLayouts)
 app.set("layout", "layouts/layout")
 
-/* ***********************
- * Routes
- *************************/
+/* Routes*/
 const staticRoutes = require("./routes/static")
 app.use(staticRoutes)
 
@@ -37,14 +25,10 @@ app.use(staticRoutes)
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" })
 })
-/* ***********************
- * Local Server Information
- *************************/
+/* **Local Server Information**/
 const PORT = process.env.PORT || 3000
 
-/* ***********************
- * Start Server
- *************************/
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
